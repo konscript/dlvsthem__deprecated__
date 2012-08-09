1c51947a916f4b5b5491924c23b4a8eccad5c99a
Søren Louv-Jansen <sorenlouv@gmail.com>
2012-08-09 17:30:34 +0200

    Front page with dynamic country

--- wp-content/themes/whitelight/includes/homepage-blog-panel.php
+++ wp-content/themes/whitelight/includes/homepage-blog-panel.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Homepage Blog Panel
  */
@@ -11,6 +12,7 @@
 	
 	$settings = array(
 					'blog_area_content' => 'blog',
+					'blog_area_title' => '',
 					'blog_area_entries' => 3,
 					'blog_area_page' => ''
 					);
@@ -21,44 +23,56 @@
 		<div class="home-blog fix">
 			
 			<section id="" class=""> 
-			
-			<?php
-				if ( $settings['blog_area_content'] == 'page' ) {
-					$page_id = $settings['blog_area_page'];
-					query_posts( array( 'post_type' => 'page', 'page_id' => $page_id, 'posts_per_page' => 1, ) );
-				} else {
-					$number_of_features = $settings['blog_area_entries'];
-    				$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1; 
-					/* The Query. */
-					query_posts( array( 'post_type' => 'post', 'posts_per_page' => $number_of_features, 'paged' => $paged, 'order' => 'DESC', 'orderby' => 'date' ) );
-				} // End If Statement
-        		if ( have_posts() ) : $count = 0;
-        	?>
-        	
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); $count++; ?>
-			
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						if ( $settings['blog_area_content'] == 'page' ) {
-							get_template_part( 'content', 'page' );
-						} else {
-							get_template_part( 'content', get_post_format() );
-						}
-					?>
-			
-				<?php endwhile; ?>
-			
-			<?php else : ?>
-        	
-        	    <article <?php post_class(); ?>>
-        	        <p><?php _e( 'Sorry, no posts matched your criteria.', 'woothemes' ); ?></p>
-        	    </article><!-- /.post -->
-        	
-        	<?php endif; ?>
+
+				<div id="map-wrapper">
+
+					<h3><?php echo $settings['blog_area_title'] ?></h3>
+				
+					<div class="map-form-container">
+						<form method="GET" class="map-form" action="<?php bloginfo('wpurl'); ?>">
+						  <select name="Country" id="country-selector">
+							<option value="" selected="selected">Vælg land</option>
+							<?php $countries = getCountries(); ?>	
+							<?php foreach($countries as $country): ?>
+								<?php 
+									$country_name = $country->post_title;
+									$country_slug = get_permalink($country->ID);				
+									$country_id = $country->ID;
+								?>					
+							<option value="<?= $country_slug; ?>"><?=$country_name; ?></option>					
+							<?php endforeach; ?>
+							</select>
+						  <input type="Submit" value="Find">
+						</form>
+						<span class="map-text">eller klik på kortet:</span>
+					</div>
+
+					 <div id="map-continents">
+					 <ul class="continents">
+					  <li class="c1"><a href="<?php bloginfo('wpurl'); ?>/region/africa">Africa</a></li>
+					  <li class="c2"><a href="<?php bloginfo('wpurl'); ?>/region/asia">Asien</a></li>
+					  <li class="c3"><a href="<?php bloginfo('wpurl'); ?>/region/oceania">Oceanien</a></li>
+					  <li class="c4"><a href="<?php bloginfo('wpurl'); ?>/region/europe">Europa</a></li>
+					  <li class="c5"><a href="<?php bloginfo('wpurl'); ?>/region/north-america">Nord Amerika</a></li>
+					  <li class="c6"><a href="<?php bloginfo('wpurl'); ?>/region/south-america">Syd Amerika</a></li>
+					 </ul>
+					</div>
+
+				</div>		
+
+				<script type="text/javascript">
+				jQuery.noConflict();
+				(function($){
+					$(document).ready(function() {	
+						$('#map-continents').cssMap({'size' : 540});
+					});
+				})(jQuery);
+					
+				</script>
+
+
+				<?php wp_enqueue_script('jquery.cssmap.js', get_template_directory_uri() . '/includes/js/dlvs/jquery.cssmap.js' );?>
+				<link rel="stylesheet" type="text/css" media="screen,projection" href="<?php echo get_template_directory_uri(); ?>/includes/css/map-continents/cssmap-continents.css" />
 			
 			<?php woo_pagenav();?>
         	        
