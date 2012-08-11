9ae01665f1706a1b3ca8fba7b99476b33566162c
laander <la@laander.com>
2012-08-11 00:36:23 -0700

    traveladvice automatically adds subpages to sidebar menu

--- wp-content/themes/whitelight/template-advice.php
+++ wp-content/themes/whitelight/template-advice.php
@@ -1,9 +1,25 @@
 <?php /* Template Name: Advice */ ?>
 <?php get_header(); ?>
+<?php
+// Sidebar menu
+if($post->post_parent) {
+	$args = array(
+		'title_li'=> '&nbsp;',
+		'child_of'=> $post->post_parent,
+		'echo' => false,
+	);
+} else {
+	$args = array(
+		'title_li'=> '&nbsp;',
+		'child_of'=> $post->ID,
+		'echo' => false,
+	);	
+}
+$sidebar_menu = wp_list_pages( $args ); ?>
 
 <div id="content">
 	<div class="page col-full">
-		<?php sidebar(true, true, false); ?>
+		<?php sidebar($sidebar_menu, true, false); ?>
 		<section id="main" class="col-left">	
 
 	        <?php
